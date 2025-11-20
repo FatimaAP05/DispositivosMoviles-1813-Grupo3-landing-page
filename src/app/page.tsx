4c0f2845c60b643a2b7bdf49f8efa39275b70bfc
Fatima andrea asmad padilla <asmadpadillaf@gmail.com>
2025-11-20 16:48:49 +0000

    la seccion FREQUENTLY ASKED QUESTIONS va despues de Everything You Need

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,10 +17,10 @@ export default function Home() {
       <main className="flex-1">
         <Hero />
         <Features />
+        <Faq />
         <Screenshots />
         <AboutProduct />
         <AboutTeam />
-        <Faq />
         <Contact />
       </main>
       <Footer />
